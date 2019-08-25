b204a28cce4db99b203d0c1bc639750e098d4273
odan <d.opitz@outlook.com>
2019-08-25 21:31:41 +0200

    Update settings

--- config/middleware.php
+++ config/middleware.php
@@ -6,7 +6,6 @@ use Slim\App;
 
 return static function (App $app) {
     $container = $app->getContainer();
-    $settings = $container->get('settings');
 
     // Add global middleware to app
     $app->addRoutingMiddleware();
@@ -16,9 +15,10 @@ return static function (App $app) {
     $app->add(LocaleSessionMiddleware::class);
 
     // Error handler
-    $displayErrorDetails = (bool)$settings['error_handler_middleware']['display_error_details'];
-    $logErrors = (bool)$settings['error_handler_middleware']['log_errors'];
-    $logErrorDetails = (bool)$settings['error_handler_middleware']['log_error_details'];
+    $settings = $container->get('settings')['error_handler_middleware'];
+    $displayErrorDetails = (bool)$settings['display_error_details'];
+    $logErrors = (bool)$settings['log_errors'];
+    $logErrorDetails = (bool)$settings['log_error_details'];
 
     $app->addErrorMiddleware($displayErrorDetails, $logErrors, $logErrorDetails);
 };
