b13a6d146258503c66d7271f268080db50a96689
odan <d.opitz@outlook.com>
2019-10-12 18:28:38 +0200

    Update schema

--- resources/migrations/00000000000000_init.php
+++ resources/migrations/00000000000000_init.php
@@ -1,83 +1,86 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
 use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
 
 class Init extends AbstractMigration
 {
+    /**
+     * Change.
+     *
+     * @return void
+     */
     public function change()
     {
-        $this->execute("ALTER DATABASE CHARACTER SET 'utf8';");
-        $this->execute("ALTER DATABASE COLLATE='utf8_unicode_ci';");
+        $this->execute("ALTER DATABASE CHARACTER SET 'utf8mb4';");
+        $this->execute("ALTER DATABASE COLLATE='utf8mb4_unicode_ci';");
         $this->table('users', [
             'id' => false,
             'primary_key' => ['id'],
             'engine' => 'InnoDB',
-            'encoding' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'encoding' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'comment' => '',
             'row_format' => 'DYNAMIC',
         ])
             ->addColumn('id', 'integer', [
                 'null' => false,
                 'limit' => MysqlAdapter::INT_REGULAR,
-                'precision' => '10',
                 'identity' => 'enable',
             ])
             ->addColumn('username', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'id',
             ])
             ->addColumn('password', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'username',
             ])
             ->addColumn('email', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'password',
             ])
             ->addColumn('first_name', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'email',
             ])
             ->addColumn('last_name', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'first_name',
             ])
             ->addColumn('role', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'last_name',
             ])
             ->addColumn('locale', 'string', [
                 'null' => true,
                 'limit' => 255,
-                'collation' => 'utf8_unicode_ci',
-                'encoding' => 'utf8',
+                'collation' => 'utf8mb4_unicode_ci',
+                'encoding' => 'utf8mb4',
                 'after' => 'role',
             ])
             ->addColumn('enabled', 'boolean', [
                 'null' => false,
                 'default' => '0',
                 'limit' => MysqlAdapter::INT_TINY,
-                'precision' => '3',
                 'after' => 'locale',
             ])
             ->addColumn('created_at', 'datetime', [
@@ -87,7 +90,6 @@ class Init extends AbstractMigration
             ->addColumn('created_user_id', 'integer', [
                 'null' => true,
                 'limit' => MysqlAdapter::INT_REGULAR,
-                'precision' => '10',
                 'after' => 'created_at',
             ])
             ->addColumn('updated_at', 'datetime', [
@@ -97,7 +99,6 @@ class Init extends AbstractMigration
             ->addColumn('updated_user_id', 'integer', [
                 'null' => true,
                 'limit' => MysqlAdapter::INT_REGULAR,
-                'precision' => '10',
                 'after' => 'updated_at',
             ])
             ->addIndex(['username'], [
--- resources/migrations/schema.sql
+++ resources/migrations/schema.sql
@@ -5,17 +5,17 @@ CREATE TABLE `phinxlog` (
   `end_time` timestamp NULL DEFAULT NULL,
   `breakpoint` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `role` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `locale` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `locale` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime DEFAULT NULL,
   `created_user_id` int(11) DEFAULT NULL,
@@ -25,4 +25,4 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`),
   KEY `created_user_id` (`created_user_id`),
   KEY `updated_user_id` (`updated_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
