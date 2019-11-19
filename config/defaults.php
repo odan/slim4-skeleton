56c82ff72b5389a6f4bf242e756f2581c71aa494
odan <d.opitz@outlook.com>
2019-11-19 17:47:51 +0100

    Update pdo settings

--- config/defaults.php
+++ config/defaults.php
@@ -94,10 +94,13 @@ $settings['db'] = [
     'encoding' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'flags' => [
+        // Turn off persistent connections
         PDO::ATTR_PERSISTENT => false,
         // Enable exceptions
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // Set default fetch mode
+        // Emulate prepared statements
+        PDO::ATTR_EMULATE_PREPARES => true,
+        // Set default fetch mode to array
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ],
 ];
