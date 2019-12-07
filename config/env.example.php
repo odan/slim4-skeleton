0c89af8f010f9ff16f746f66088be84c628d0186
odan <d.opitz@outlook.com>
2019-12-07 13:53:41 +0100

    Update github actions

--- config/env.example.php
+++ config/env.example.php
@@ -32,4 +32,4 @@ $settings['twig']['cache_enabled'] = false;
 // Database
 $settings['db']['database'] = 'test';
 $settings['db']['username'] = 'root';
-$settings['db']['password'] = '';
+$settings['db']['password'] = 'root';
