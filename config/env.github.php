8b1858ddaed6828ff27fa59b4226e014d961b1f8
odan <d.opitz@outlook.com>
2019-12-07 15:55:31 +0100

    Update github actions

---
+++ config/env.github.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Environment specific application configuration.
+ *
+ * You should store all secret information (username, password, token) here.
+ *
+ * Make sure the env.php file is added to your .gitignore
+ * so it is not checked-in the code
+ *
+ * Place the env.php _outside_ the project root directory, to protect against
+ * overwriting at deployment.
+ *
+ * This usage ensures that no sensitive passwords or API keys will
+ * ever be in the version control history so there is less risk of
+ * a security breach, and production values will never have to be
+ * shared with all project collaborators.
+ */
+require __DIR__ . '/development.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+$settings['env'] = 'development';
+
+$settings['error_handler_middleware']['log_errors'] = false;
+$settings['logger']['level'] = \Monolog\Logger::DEBUG;
+$settings['assets']['minify'] = 0;
+$settings['locale']['cache'] = null;
+$settings['twig']['cache_enabled'] = false;
+
+// Database
+$settings['db']['database'] = 'test';
+$settings['db']['username'] = 'root';
+$settings['db']['password'] = 'root';
