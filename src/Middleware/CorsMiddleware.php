39de8fa14862f0d06fb3e13561fbc94f2cc2fe2a
odan <d.opitz@outlook.com>
2020-01-05 13:57:51 +0100

    Add CORS middleware

---
+++ src/Middleware/CorsMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Routing\RouteContext;
+
+/**
+ * CORS middleware.
+ *
+ * Allows CORS preflight from any domain.
+ */
+final class CorsMiddleware implements MiddlewareInterface
+{
+    /**
+     * Invoke middleware.
+     *
+     * @param ServerRequestInterface $request The request
+     * @param RequestHandlerInterface $handler The handler
+     *
+     * @return ResponseInterface The response
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $routeContext = RouteContext::fromRequest($request);
+        $routingResults = $routeContext->getRoutingResults();
+        $methods = $routingResults->getAllowedMethods();
+        $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
+
+        $response = $handler->handle($request);
+
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+        $response = $response->withHeader('Access-Control-Allow-Methods', implode(',', $methods));
+        $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
+
+        // Allow Ajax CORS requests with Authorization header
+        $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
