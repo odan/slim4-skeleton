96ccbf213d56c0a25e054188c87246806fe0ec06
odan <d.opitz@outlook.com>
2020-04-17 09:00:44 +0200

    Fix spinner module

--- templates/layout/spinner.js
+++ templates/layout/spinner.js
@@ -1,15 +1,19 @@
 require('./spinner.css');
 
-export function showLoading() {
+const showLoading = function () {
     hideLoading();
     const spinner = document.createElement('div');
     spinner.setAttribute('id', 'spinner');
     document.body.appendChild(spinner);
 };
 
-export function hideLoading() {
+const hideLoading = function () {
     const spinner = document.getElementById("spinner");
     if (spinner) {
         spinner.remove();
     }
 };
+
+module.exports.showLoading = showLoading;
+module.exports.hideLoading = hideLoading;
+
