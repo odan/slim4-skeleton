a52c1666568ac93b3e1a0b4efa7c3b7606dfa4bf
odan <d.opitz@outlook.com>
2020-07-04 15:28:32 +0200

    Add console env option

--- bin/console.php
+++ bin/console.php
@@ -2,19 +2,27 @@
 
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputOption;
 
-if (isset($_SERVER['REQUEST_METHOD'])) {
-    echo "Only CLI allowed. Script stopped.\n";
-    exit(1);
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$env = (new ArgvInput())->getParameterOption(['--env', '-e'], 'development');
+
+if ($env) {
+    $_ENV['APP_ENV'] = $env;
 }
 
 /** @var ContainerInterface $container */
 $container = (require __DIR__ . '/../config/bootstrap.php')->getContainer();
 
-$commands = $container->get('settings')['commands'];
 $application = new Application();
 
-foreach ($commands as $class) {
+$application->getDefinition()->addOption(
+    new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'development')
+);
+
+foreach ($container->get('settings')['commands'] as $class) {
     $application->add($container->get($class));
 }
 
