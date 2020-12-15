18f32c994bfb302983670d046f7647e4fbc5693b
odan <d.opitz@outlook.com>
2020-12-15 20:33:18 +0100

    Add tests

--- tests/TestCase/Action/Login/LoginSubmitActionTest.php
+++ tests/TestCase/Action/Login/LoginSubmitActionTest.php
@@ -23,9 +23,11 @@ class LoginSubmitActionTest extends TestCase
      */
     public function testLoginAdminAction(): void
     {
-        $this->insertFixtures([
-            UserFixture::class,
-        ]);
+        $this->insertFixtures(
+            [
+                UserFixture::class,
+            ]
+        );
 
         $request = $this->createJsonRequest('POST', '/login', ['username' => 'admin', 'password' => 'admin']);
         $response = $this->app->handle($request);
@@ -52,9 +54,11 @@ class LoginSubmitActionTest extends TestCase
      */
     public function testLoginUserAction(): void
     {
-        $this->insertFixtures([
-            UserFixture::class,
-        ]);
+        $this->insertFixtures(
+            [
+                UserFixture::class,
+            ]
+        );
 
         $request = $this->createJsonRequest('POST', '/login', ['username' => 'user', 'password' => 'user']);
         $response = $this->app->handle($request);
@@ -72,5 +76,26 @@ class LoginSubmitActionTest extends TestCase
         $this->assertSame(2, $user->id);
         $this->assertSame('de_DE', $user->locale);
         $this->assertSame('user@example.com', $user->email);
+    }
+
+    /**
+     * Test.
+     *
+     * @return void
+     */
+    public function testLoginFailed(): void
+    {
+        $this->insertFixtures(
+            [
+                UserFixture::class,
+            ]
+        );
+
+        $request = $this->createJsonRequest('POST', '/login', ['username' => 'foo', 'password' => 'foo']);
+        $response = $this->app->handle($request);
+
+        $this->assertSame(302, $response->getStatusCode());
+        $this->assertSame('/login', $response->getHeaderLine('Location'));
+        $this->assertEquals('', (string)$response->getBody());
     }
 }
