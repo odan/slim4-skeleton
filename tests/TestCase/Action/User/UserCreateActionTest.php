43e5273e5c755168f51a7cebbc0a3564203720d4
odan <d.opitz@outlook.com>
2020-12-15 22:20:28 +0100

    Optimize tests

--- tests/TestCase/Action/User/UserCreateActionTest.php
+++ tests/TestCase/Action/User/UserCreateActionTest.php
@@ -32,5 +32,26 @@ class UserCreateActionTest extends TestCase
         $this->assertJsonData(['user_id' => 1], $response);
         $this->assertSame('application/json', $response->getHeaderLine('Content-Type'));
         $this->assertSame(201, $response->getStatusCode());
+
+        $this->assertTableRowCount(1, 'users');
+
+        $expected = [
+            'id' => '1',
+            'username' => 'admin',
+            'password' => null,
+            'email' => 'mail@example.com',
+            'first_name' => null,
+            'last_name' => null,
+            'role' => null,
+            'locale' => null,
+            'enabled' => '0',
+            'created_at' => '2020-01-01 00:00:00',
+            'created_user_id' => null,
+            'updated_at' => null,
+            'updated_user_id' => null,
+        ];
+
+        $this->assertTableRow($expected, 'users', 1);
+        $this->assertTableRowValue('1', 'users', 1, 'id');
     }
 }
