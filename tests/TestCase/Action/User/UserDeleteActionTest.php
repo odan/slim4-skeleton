2fbffc382b0500e373e38d183ec14059f56054ee
Daniel Opitz <d.opitz@outlook.com>
2021-03-10 11:12:23 +0100

    Fix delete for parallel execution

--- tests/TestCase/Action/User/UserDeleteActionTest.php
+++ tests/TestCase/Action/User/UserDeleteActionTest.php
@@ -39,22 +39,4 @@ class UserDeleteActionTest extends TestCase
         $this->assertTableRowCount(1, 'users');
         $this->assertTableRowNotExists('users', 1);
     }
-
-    /**
-     * Test.
-     *
-     * @return void
-     */
-    public function testDeleteInvalidUser(): void
-    {
-        $this->insertFixtures([UserFixture::class]);
-
-        $request = $this->createJsonRequest('DELETE', '/api/users/99');
-        $request = $this->withHttpBasicAuth($request);
-
-        $response = $this->app->handle($request);
-
-        // Check response
-        $this->assertSame(StatusCodeInterface::STATUS_BAD_REQUEST, $response->getStatusCode());
-    }
 }
