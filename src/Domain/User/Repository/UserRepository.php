69912d0bc8b707323f692edadbbd97a0fd0d8f2c
Daniel Opitz <d.opitz@outlook.com>
2021-03-10 11:07:37 +0100

    Fix delete for parallel execution

--- src/Domain/User/Repository/UserRepository.php
+++ src/Domain/User/Repository/UserRepository.php
@@ -118,19 +118,13 @@ final class UserRepository
      *
      * @param int $userId The user id
      *
-     * @throws DomainException
-     *
      * @return void
      */
     public function deleteUserById(int $userId): void
     {
-        $statement = $this->queryFactory->newDelete('users')
+        $this->queryFactory->newDelete('users')
             ->andWhere(['id' => $userId])
             ->execute();
-
-        if (!$statement->count()) {
-            throw new DomainException(sprintf('Cannot delete user: %s', $userId));
-        }
     }
 
     /**
