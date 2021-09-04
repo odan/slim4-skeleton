d1c7d8fd59713c5814a5ca0dffe2cdb1350c108d
Daniel Opitz <d.opitz@outlook.com>
2021-09-04 12:03:43 +0200

    Add RouteTestTrait

---
+++ tests/Traits/RouteTestTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Test\Traits;
+
+/**
+ * Slim App Route Test Trait.
+ */
+trait RouteTestTrait
+{
+    /**
+     * Build the path for a named route including the base path.
+     *
+     * @param string $routeName Route name
+     * @param string[] $data Named argument replacement data
+     * @param string[] $queryParams Optional query string parameters
+     *
+     * @return string route with base path
+     */
+    protected function urlFor(string $routeName, array $data = [], array $queryParams = []): string
+    {
+        return $this->app->getRouteCollector()->getRouteParser()->urlFor($routeName, $data, $queryParams);
+    }
+}
