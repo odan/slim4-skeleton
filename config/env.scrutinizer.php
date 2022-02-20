ad0e03eefd92bae1ded2535083456cf95893d61c
Daniel Opitz <d.opitz@outlook.com>
2022-02-20 11:02:52 +0100

    Fix scrutinizer settings

---
+++ config/env.scrutinizer.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/local.test.php';
+
+// Database
+$settings['db']['host'] = '127.0.0.1';
+$settings['db']['database'] = 'slim_skeleton_test';
+$settings['db']['username'] = 'root';
+$settings['db']['password'] = '';
+
+$settings['phoenix']['environments']['local']['db_name'] = $settings['db']['database'];
+$settings['phoenix']['environments']['local']['username'] = $settings['db']['username'];
+$settings['phoenix']['environments']['local']['password'] = $settings['db']['password'];
