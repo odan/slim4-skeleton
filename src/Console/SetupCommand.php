258bbf5011d08daaff1d665c16845928d9c95934
Daniel Opitz <d.opitz@outlook.com>
2022-07-31 17:53:13 +0200

    Update setup

--- src/Console/SetupCommand.php
+++ src/Console/SetupCommand.php
@@ -7,9 +7,20 @@ use PDO;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use UnexpectedValueException;
 
 final class SetupCommand extends Command
 {
+    private string $dbHost;
+    private string $dbPort;
+    private string $dbName;
+    private string $dbNameTest;
+    private string $dbUsername;
+    private string $dbPassword;
+    private array $pdoOptions = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ];
+
     protected function configure(): void
     {
         parent::configure();
@@ -20,96 +31,57 @@ final class SetupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('<info>**** Slim Setup ****</info>');
-
-        if (file_exists(__DIR__ . '/../../config/env.php')) {
-            $output->writeln('<error>The file config/env.php already exists.</error>');
-
-            return 1;
-        }
-
-        $dbHost = readline('Enter the database hostname or ip address [127.0.0.1]:') ?: '127.0.0.1';
-        $dbPort = readline('Enter the database port [3306]:') ?: '3306';
-        $dbName = readline('Enter the DEV database name [slim_example]:') ?: 'slim_example';
-        $dbNameTest = readline('Enter the TEST database name [slim_example_test]:') ?: 'slim_example_test';
-        $dbUsername = readline('Enter the database username [root]:') ?: 'root';
-        $dbPassword = readline('Enter the database password [empty]:') ?: '';
-
-        $pdoOptions = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ];
+        $output->writeln('<info>*** Slim Setup ***</info>');
 
         try {
-            $output->writeln('Open TEST database connection');
-            $pdoTest = new PDO("mysql:host=$dbHost;charset=utf8mb4", $dbUsername, $dbPassword, $pdoOptions);
+            if (file_exists(__DIR__ . '/../../config/env.php')) {
+                throw new UnexpectedValueException('The file config/env.php already exists.');
+            }
+
+            $this->askDbParameters();
+
+            $output->writeln('Connect to database server');
+            $pdo = $this->connectToDatabase();
+
+            if ($this->existsDatabase($pdo, $this->dbNameTest)) {
+                throw new UnexpectedValueException("The database [$this->dbNameTest] already exists.");
+            }
+
+            if ($this->existsDatabase($pdo, $this->dbName)) {
+                throw new UnexpectedValueException("The database [$this->dbName] already exists.");
+            }
 
             $output->writeln('Create TEST database');
-            $pdoTest->exec($this->createTableSql($dbNameTest));
-
-            $pdoTest = null;
-        } catch (Exception $exception) {
-            $output->writeln('<error>TEST database connection failed.</error>');
-            $output->writeln($exception->getMessage());
-
-            return 1;
-        }
-
-        $output->writeln('<info>TEST database connection successfully</info>');
-
-        try {
-            $output->writeln('Connect to DEV database server');
-            $pdo = new PDO("mysql:host=$dbHost;charset=utf8mb4", $dbUsername, $dbPassword, $pdoOptions);
+            $this->createDatabase($pdo, $this->dbNameTest);
+            $output->writeln('<info>TEST database created successfully</info>');
 
             $output->writeln('Create DEV database');
-            $pdo->exec($this->createTableSql($dbName));
-            $pdo->exec($this->useSql($dbName));
+            $this->createDatabase($pdo, $this->dbName);
+
+            $output->writeln('Create DEV tables');
+            $pdo->exec($this->createTableSql($this->dbName));
+            $pdo->exec($this->useSql($this->dbName));
+            $output->writeln('<info>DEV database created successfully</info>');
+
+            $output->writeln('Import database schema');
+            $pdo->exec((string)file_get_contents(__DIR__ . '/../../resources/schema/schema.sql'));
+
+            $output->writeln('Create config/env.php file');
+            $this->safeEnvFile();
+
+            $output->writeln('<info>Setup was successfully!</info>');
+            $output->writeln('To start all tests, run: composer test');
+            $output->writeln('To start the internal webserver, run: composer start');
+
+            return 0;
         } catch (Exception $exception) {
-            $output->writeln('<error>DEV database connection failed.</error>');
-            $output->writeln($exception->getMessage());
+            $output->writeln(sprintf('<error>%s</error>', $exception->getMessage()));
+            if ($exception->getPrevious()) {
+                $output->writeln($exception->getPrevious()->getMessage());
+            }
 
             return 1;
         }
-
-        $output->writeln('<info>DEV database created successfully</info>');
-
-        $output->writeln('Import database schema');
-        $pdo->exec((string)file_get_contents(__DIR__ . '/../../resources/schema/schema.sql'));
-        $pdo = null;
-
-        $output->writeln('Create config/env.php file');
-
-        $code = [
-            '<?php',
-            '',
-            '// Secret credentials',
-            '',
-            'return function (array $settings): array {',
-            '',
-            "    \$settings['db']['host'] = '$dbHost';",
-            "    \$settings['db']['port'] = '$dbPort';",
-            "    \$settings['db']['username'] = '$dbUsername';",
-            "    \$settings['db']['password'] = '$dbPassword';",
-            '',
-            "    if (defined('PHPUNIT_COMPOSER_INSTALL')) {",
-            '        // PHPUnit test database',
-            "        \$settings['db']['database'] = '$dbNameTest';",
-            '    } else {',
-            '        // Local dev database',
-            "        \$settings['db']['database'] = '$dbName';",
-            '    }',
-            '',
-            '    return $settings;',
-            '};',
-            '',
-        ];
-
-        file_put_contents(__DIR__ . '/../../config/env.php', implode("\n", $code));
-
-        $output->writeln('<info>Setup was successfully!</info>');
-        $output->writeln('To start all tests, run: composer test');
-        $output->writeln('To start the internal webserver, run: composer start');
-
-        return 0;
     }
 
     private function createTableSql(string $table): string
@@ -120,5 +92,71 @@ final class SetupCommand extends Command
     private function useSql(string $database): string
     {
         return sprintf('USE `%s`;', $database);
+    }
+
+    private function askDbParameters(): void
+    {
+        $this->dbHost = (string)readline('Enter the database hostname or ip address [127.0.0.1]:') ?: '127.0.0.1';
+        $this->dbPort = (string)readline('Enter the database port [3306]:') ?: '3306';
+        $this->dbName = (string)readline('Enter the DEV database name [slim]:') ?: 'slim';
+        $this->dbNameTest = (string)readline(
+            'Enter the TEST database name [slim_test]:'
+        ) ?: 'slim_test';
+        $this->dbUsername = (string)readline('Enter the database username [root]:') ?: 'root';
+        $this->dbPassword = (string)readline('Enter the database password [empty]:') ?: '';
+    }
+
+    private function safeEnvFile(): void
+    {
+        $code = [
+            '<?php',
+            '',
+            '// Secret credentials',
+            '',
+            'return function (array $settings): array {',
+            '',
+            "    \$settings['db']['host'] = '$this->dbHost';",
+            "    \$settings['db']['port'] = '$this->dbPort';",
+            "    \$settings['db']['username'] = '$this->dbUsername';",
+            "    \$settings['db']['password'] = '$this->dbPassword';",
+            '',
+            "    if (defined('PHPUNIT_COMPOSER_INSTALL')) {",
+            '        // PHPUnit test database',
+            "        \$settings['db']['database'] = '$this->dbNameTest';",
+            '    } else {',
+            '        // Local dev database',
+            "        \$settings['db']['database'] = '$this->dbName';",
+            '    }',
+            '',
+            '    return $settings;',
+            '};',
+            '',
+        ];
+
+        file_put_contents(__DIR__ . '/../../config/env.php', implode("\n", $code));
+    }
+
+    private function createDatabase(PDO $pdo, string $database): void
+    {
+        $pdo->exec($this->createTableSql($database));
+    }
+
+    private function connectToDatabase(): PDO
+    {
+        return new PDO(
+            "mysql:host=$this->dbHost;charset=utf8mb4",
+            $this->dbUsername,
+            $this->dbPassword,
+            $this->pdoOptions
+        );
+    }
+
+    private function existsDatabase(PDO $pdo, string $database): bool
+    {
+        $statement = $pdo->prepare('SELECT SCHEMA_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = :database');
+        $statement->bindValue('database', $database);
+        $statement->execute();
+
+        return !empty($statement->fetch());
     }
 }
