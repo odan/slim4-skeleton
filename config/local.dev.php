ec7290fae8f056234ccd6a798490aca6c0dc2f69
Benediktas Rukas <benediktasrukas@registrucentras.lt>
2022-09-19 15:38:46 +0300

    Add full error reporting to the Dev env

--- config/local.dev.php
+++ config/local.dev.php
@@ -3,6 +3,11 @@
 // Dev environment
 
 return function (array $settings): array {
+    // Error reporting
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+
     $settings['error']['display_error_details'] = true;
     $settings['logger']['level'] = \Monolog\Level::Debug;
 
