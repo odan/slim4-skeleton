4ff2ba21a8c724c4163ad132ace73739528c37bb
Daniel Opitz <d.opitz@outlook.com>
2022-11-20 13:24:41 +0100

    Add symfony/dotenv example

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,21 @@
 <?php
 
-// Load .env file if located above the project root directory
-// \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../')->safeLoad();
+// Load .env file, if exists
+use Symfony\Component\Dotenv\Dotenv;
+
+$envFiles = [
+    __DIR__ . '/.env',
+    __DIR__ . '/../../.env'
+];
+
+if (class_exists(Dotenv::class)) {
+    $dotenv = new Dotenv();
+    foreach ($envFiles as $envFile) {
+        if (file_exists($envFile)) {
+            $dotenv->load($envFile);
+        }
+    }
+}
 
 // Detect environment
 $_ENV['APP_ENV'] = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'dev';
