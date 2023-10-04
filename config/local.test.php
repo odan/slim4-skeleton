b2966b3497d78ba3ad2bfb2dd30bd1d8304c2b16
Daniel Opitz <d.opitz@outlook.com>
2023-10-04 22:57:47 +0200

    Remove unused test logger settings #122

--- config/local.test.php
+++ config/local.test.php
@@ -9,11 +9,6 @@ return function (array $settings): array {
     // Database
     $settings['db']['database'] = 'slim_skeleton_test';
 
-    // Mocked Logger settings
-    $settings['logger'] = [
-        'test' => new \Monolog\Handler\TestHandler(),
-    ];
-
     // API credentials for phpunit
     $settings['api_auth'] = [
         // Allow http for testing
